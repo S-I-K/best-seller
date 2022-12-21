440667532461aa8afa8c97163d43639039e9b8ff
sik <daesik99@naver.com>
2022-12-21 18:02:05 +0900

    update/ header, main slide library use

--- js/script.js
+++ js/script.js
@@ -1,3 +1,10 @@
 $(() => {
-  console.log($);
+  /* main swiper slide */
+  var mainSwiper = new Swiper("#main_slide", {
+    loop: true,
+    autoplay: {
+      delay: 4000,
+      disableOnInteraction: true,
+    },
+  });
 });
